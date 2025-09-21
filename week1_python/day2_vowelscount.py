3bd52d18b46c5f32c01de7467702bb654528a804
Priya Kumari <itsmepriya290@gmail.com>
2025-09-21 14:53:05 +0530

    Updated it by adding pythonic approach

--- week1_python/day2_vowelscount.py
+++ week1_python/day2_vowelscount.py
@@ -10,8 +10,15 @@ class Solution:
             if n in vowels_set:
                 c+=1
         return c
+    
+    def countvowels(self,names:str)->int:
+        vowel_set={'A','E','I','O','U','a','e','i','o','u'}
+        return sum(n in vowel_set for n in names)
 
 s=Solution()
 name = input("Enter your name : ")
 vowels = "AEIOUaeiou"
 print(f"The total vowels in {name} is ",s.countVowels(name,vowels))
+print("pythonic Approach ")
+names = input("Enter your name for pythonic approach ")
+print(f"Total Vowel count is ",s.countvowels(names))
